30236bdab51a234d325edf1648be9dbafdbfafbb
Dima Esipenko <mif83@mail.ru>
2017-05-31 16:06:21 +0300

    add form

--- js/my.js
+++ js/my.js
@@ -1,11 +1,28 @@
 /**
  * Created by dima on 30.05.2017.
  */
+var regDataForm = {
+    city:["Dnepr","Kiev","Kharkiv","Odessa","Zaporozhye","Kamenskoe"],
+    gender:["male","female"],
+    bloodType:["first (O)","second (A)","third (B)","forth (AB)"],
+    rhesus:["positive","negative"]
+
+};
+
+Vue.component("v-form", {
+    template: "#reg-form",
+    data: function(){
+        return regDataForm;
+    }
+});
+
 var app = new Vue(
     {
-        el:"#app",
+        el:"#registration",
         data:{
-            message:"Hi"
+            ucity:"",
+            ugender:""
         }
+
     }
 );
